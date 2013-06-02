672c478b8e3a44118c68c1569df770c79ea03f53
Saumitra <saumitra@yahoo-inc.com>
2013-06-01 23:53:40 -0700

    Add single onClick function instead of 3 event listeners

--- src/com/yahoo/tipcalculator/MainActivity.java
+++ src/com/yahoo/tipcalculator/MainActivity.java
@@ -1,9 +1,10 @@
 package com.yahoo.tipcalculator;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,52 +25,63 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        etBillAmount = (EditText) findViewById(R.id.etBillAmount);
-        tvTipAmount  = (TextView) findViewById(R.id.tvTipText);
-        
         bTip10       = (Button)   findViewById(R.id.bTip10);
         bTip15       = (Button)   findViewById(R.id.bTip15);
-        bTip20       = (Button)   findViewById(R.id.bTip20);      
+        bTip20       = (Button)   findViewById(R.id.bTip20);
         
-        bTip10.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				tvTipAmount.setText("Tip is: " + calculateTip(etBillAmount.getText().toString(), 10));
-				lastTipPercent = 10;
-			}
-		});
+        bTip10.setTag(10);
+        bTip15.setTag(15);
+        bTip20.setTag(20);
         
-        bTip15.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				tvTipAmount.setText("Tip is: " + calculateTip(etBillAmount.getText().toString(), 15));
-				lastTipPercent = 15;
-			}
-		});
-        
-        bTip20.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				tvTipAmount.setText("Tip is: " + calculateTip(etBillAmount.getText().toString(), 20));
-				lastTipPercent = 20;
-			}
-		});
+//        bTip10.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				tvTipAmount.setText("Tip is: " + calculateTip(etBillAmount.getText().toString(), Double.parseDouble(bTip10.getTag().toString())));
+//				lastTipPercent = 10;
+//				inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+//			}
+//		});
+//        
+//        bTip15.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				tvTipAmount.setText("Tip is: " + calculateTip(etBillAmount.getText().toString(), 15));
+//				lastTipPercent = 15; 
+//				inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+//			}
+//		});
+//        
+//        bTip20.setOnClickListener(new View.OnClickListener() {
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				tvTipAmount.setText("Tip is: " + calculateTip(etBillAmount.getText().toString(), 20));
+//				lastTipPercent = 20;
+//				
+//			}
+//		});
     }
     
-    public double calculateTip(String billAmount, double tipPercent) {
-    	return (double) Double.parseDouble(billAmount.toString()) * tipPercent / 100;
+    public void calculateTipFunction(View v) {
+    	etBillAmount = (EditText) findViewById(R.id.etBillAmount);
+    	tvTipAmount  = (TextView) findViewById(R.id.tvTipText);
+    	
+    	final InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+    	
+    	double tip = Double.parseDouble(etBillAmount.getText().toString()) * Double.parseDouble(v.getTag().toString()) / 100;
+    	
+    	tvTipAmount.setText("Tip is: " + String.format("%.2f", tip));
+    	inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
     
     
 }
